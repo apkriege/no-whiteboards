32aa3f49f278e88b527f959d85b10bc248e10179
Adam Krieger <adamkrieger87@gmail.com>
2024-01-07 23:08:40 -0500

    init

---
+++ types.go
@@ -0,0 +1,27 @@
+package main
+
+type Job struct {
+	Name      string   `json:"name"`
+	Link      string   `json:"link"`
+	Locations []string `json:"locations"`
+	Process   string   `json:"process"`
+}
+
+type Set map[string]struct{}
+
+func NewSet() Set {
+	return make(Set)
+}
+
+func (s Set) Add(element string) {
+	s[element] = struct{}{}
+}
+
+func (s Set) Exists(element string) bool {
+	_, ok := s[element]
+	return ok
+}
+
+func (s Set) Remove(element string) {
+	delete(s, element)
+}
